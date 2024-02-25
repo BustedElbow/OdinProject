d13bb6e670e8378be04421dc66cf5c598d6c77e5
bustedelbow <velozity2@gmail.com>
2024-02-25 22:58:54 +0800

    Function placeholder

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+
+function add() {
+  //add
+}
+
+function subtract() {
+  //subtract
+}
+
+function multiply() {
+  //multiply
+}
+
+function divide() {
+  //divide
+}
+
+
+function evaluate() {
+  //evalaute line
+}
