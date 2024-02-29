15ec835dd42968d692c4e14205f83cfb4ac20069
bustedelbow <velozity2@gmail.com>
2024-02-29 22:05:01 +0800

    Fixed addition logic

--- calculator/script.js
+++ calculator/script.js
@@ -19,18 +19,37 @@ function divide(numOne,numTwo) {
   return numOne / numTwo
 }
 
-function evaluate(firstNum,Operator,secondNum) {
+function operate(firstNum,Operator,secondNum) {
   //evalaute line
+  let result = 0;
 
+  switch(Operator) {
+    case 'addition':
+      result = add(firstNum, secondNum);
+      break;
+  }
+
+  return result;
 }
 
-let firstNum = null;
-let operator = null;
-let secondNum = null;
+function buttonClick(value) {
+  screenBottom.textContent += value;
+  currentNum = parseInt(screenBottom.textContent);
+}
+
+let previousNum = 0;
+let currentNum = 0;
+let operator = '';
 
 let screenBottom = document.querySelector('.screen-bottom');
 let screenTop = document.querySelector('.screen-top');
+let testBtn = document.querySelector('#test')
 
+testBtn.addEventListener('click', () => {
+  console.log(`Current Number: ${currentNum}`);
+  console.log(`Previous Number: ${previousNum}`);
+  console.log(`Operator: ${operator}`);
+})
 //Buttons
 let clearAllBtn = document.querySelector('#clearAllBtn');
 let clearBtn = document.querySelector('#clearBtn')
@@ -53,45 +72,37 @@ let equalBtn = document.querySelector('#equalBtn');
 
 clearAllBtn.addEventListener('click', () => {
   screenTop.textContent = '';
+  previousNum = 0;
+  currentNum = 0;
+  operator = '';
 })
 clearBtn.addEventListener('click', () => {
   screenBottom.textContent = '';
 })
-zeroBtn.addEventListener('click', () => {
-  screenBottom.textContent += 0;
-})
-oneBtn.addEventListener('click', () => {
-  screenBottom.textContent += 1;
-})
-twoBtn.addEventListener('click', () => {
-  screenBottom.textContent += 2;
-})
-threeBtn.addEventListener('click', () => {
-  screenBottom.textContent += 3;
-})
-fourBtn.addEventListener('click', () => {
-  screenBottom.textContent += 4;
-})
-fiveBtn.addEventListener('click', () => {
-  screenBottom.textContent += 5;
-})
-sixBtn.addEventListener('click', () => {
-  screenBottom.textContent += 6;
-})
-sevenBtn.addEventListener('click', () => {
-  screenBottom.textContent += 7;
-})
-eightBtn.addEventListener('click', () => {
-  screenBottom.textContent += 8;
-})
-nineBtn.addEventListener('click', () => {
-  screenBottom.textContent += 9;
-})
+
+oneBtn.addEventListener('click', () => {buttonClick(1)})
+twoBtn.addEventListener('click', () => {buttonClick(2)})
+threeBtn.addEventListener('click', () => {buttonClick(3)})
+fourBtn.addEventListener('click', () => {buttonClick(4)})
+fiveBtn.addEventListener('click', () => {buttonClick(5)})
+sixBtn.addEventListener('click', () => {buttonClick(6)})
+sevenBtn.addEventListener('click', () => {buttonClick(7)})
+eightBtn.addEventListener('click', () => {buttonClick(8)})
+nineBtn.addEventListener('click', () => {buttonClick(9)})
+zeroBtn.addEventListener('click', () => {buttonClick(0)})
+
+
 addBtn.addEventListener('click', () => {
+  operator = 'addition';
+  console.log(operate(previousNum, operator, currentNum));
+  previousNum = currentNum;
   screenTop.textContent += screenBottom.textContent;
   screenTop.textContent += ' + ';
   screenBottom.textContent = '';
+  currentNum = 0;
 })
+
+
 subBtn.addEventListener('click', () => {
   screenTop.textContent += screenBottom.textContent;
   screenTop.textContent += ' - ';
@@ -108,8 +119,10 @@ divideBtn.addEventListener('click', () => {
   screenBottom.textContent = '';
 })
 equalBtn.addEventListener('click', () => {
+  operate(previousNum, operator, currentNum);
   screenTop.textContent += screenBottom.textContent;
-  screenBottom.textContent = 'placeholderAnswer'
+  screenBottom.textContent =  operate(previousNum, operator, currentNum);
   console.log(screenTop.textContent)
 })
 
+console.log(add(2,3))
